40a25bc2ab9c28c36d3c6b7ac072757445a503f6
Cam Brooks <cambrooks3393@gmail.com>
2025-03-16 17:04:26 -0400

    moved debug macros to config

--- firmware/RLHT/RLHT.ino
+++ firmware/RLHT/RLHT.ino
@@ -19,26 +19,6 @@
 #include "config_hardware.h" // hardware configuration containing pin definitions
 #include "config.h"          // configuration file for constants such as i2c address and timing
 
-/**
- * @def SLICE_DEBUG
- * @brief Uncomment to enable additional debugging messages.
- */
-
-// #define SLICE_DEBUG 1
-
-#ifdef SLICE_DEBUG
-
-#define SLICE_DEBUG_PRINT(...) \
-  Serial.print(__VA_ARGS__)
-
-#define SLICE_DEBUG_PRINTLN(...) \
-  Serial.println(__VA_ARGS__)
-
-#else
-#define SLICE_DEBUG_PRINT(...)
-#define SLICE_DEBUG_PRINTLN(...)
-#endif
-
 // CRUMBS start
 CRUMBS crumbsSlice(false, I2C_ADR); // Peripheral mode, I2C address 0x08
 
--- firmware/RLHT/config.h
+++ firmware/RLHT/config.h
@@ -9,4 +9,20 @@
 #define THERMO_UPDATE_TIME_MS 300
 #define SERIAL_UPDATE_TIME_MS 1000
 
+//  Uncomment to enable additional debugging messages:
+// #define SLICE_DEBUG 1
+
+#ifdef SLICE_DEBUG
+
+#define SLICE_DEBUG_PRINT(...) \
+    Serial.print(__VA_ARGS__)
+
+#define SLICE_DEBUG_PRINTLN(...) \
+    Serial.println(__VA_ARGS__)
+
+#else
+#define SLICE_DEBUG_PRINT(...)
+#define SLICE_DEBUG_PRINTLN(...)
+#endif
+
 #endif // CONFIG_H
